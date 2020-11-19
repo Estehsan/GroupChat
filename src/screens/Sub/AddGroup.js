caae4d922953aeea7325c3d41d6aab431adc7d0f
games.huseb@gmail.com <games.huseb@gmail.com>
2020-11-20 02:50:25 +0500

    Updated Group Chat Screen

--- src/screens/Sub/AddGroup.js
+++ src/screens/Sub/AddGroup.js
@@ -1,104 +1,141 @@
-import React , {useState, useEffect, useRef} from 'react';
-import {
-    View,
-    Text,
-    ActivityIndicator,
-    StyleSheet,
-    Animated,
-    Image
-} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import Entypo from '@expo/vector-icons/Entypo';
-import Icon from '@expo/vector-icons/MaterialIcons';
-import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import * as React from "react";
+import { Text,View,Image, StyleSheet,TextInput, SafeAreaView, ScrollView, KeyboardAvoidingView } from "react-native";
 import { AntDesign,Ionicons } from '@expo/vector-icons';
 
-const AddGroup = (props) => {
-        return (
-            <LinearGradient
-            colors={["#000", "#7F8C8D"]}
-             style={styles.gradient}
-            >
-                <View style={styles.headerContainer}>
-                    <TouchableOpacity 
+import { LinearGradient } from "expo-linear-gradient";
+import { Feather } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+export default function AddGroup(props) {
+    return (
+      <SafeAreaView style={styles.container}>
+           <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={styles.container}>
+            <View style={styles.tcontainer}>
+              <TouchableOpacity 
                       onPress={() => props.navigation.goBack()}
                      >  
-                      <View><Ionicons name="ios-arrow-back" size={30} color="white" /><Text style={styles.header}>Back</Text></View>
-                     
-                   
+                      <View><Ionicons name="ios-arrow-back" size={30} color="white" /></View>                  
                      </TouchableOpacity>
-                   
+                     
+<Text style={styles.tHeading}>Add Groups</Text>
+               <TouchableOpacity 
+                      onPress={() => props.navigation.goBack()}
+                     >  
+                     </TouchableOpacity>
+            </View>
+            <View style={styles.bcontainer}>
+              <View style={styles.topbcontainer}>
+                <View style={styles.menuContainer}>
+                 <Image style={{height:180,width:200}} source={require('../../images/group_chat.png')}></Image>
+                 <Text style={styles.activemenuText}>
+                 Create Group
+               </Text>
+                <Image style={{height:90,width:89}} source={{uri:'https://static.vecteezy.com/system/resources/thumbnails/001/191/814/small/circle-abstract.png'}}/>
+                 <TextInput
+                 placeholder="Enter Group Name"
+                 placeholderTextColor="#666666" style={styles.inputs}  onChangeText={text => onChangeText(text)}
+    />
+       <TextInput
+                 placeholder="Enter Hashtag"
+                 placeholderTextColor="#666666" style={styles.inputs}  onChangeText={text => onChangeText(text)}
+    />
+       <TextInput style={{height:4}}
+                 placeholder="Enter Group Description"
+                 placeholderTextColor="#666666" style={styles.inputs}  onChangeText={text => onChangeText(text)}
+    />
+    
+                 
+                 
                 </View>
-               <Text>......</Text>
-                <View style={styles.ops}>
-                     <View style={styles.col}>
-                         <Text style={styles.day}>Add Group</Text>
-                     </View>
-                    <View style={styles.Form}> 
-                <Text>Hello</Text>
-                    </View>
-                </View>
-            </LinearGradient>
-         
-        )
-}
+              </View>
+            </View>
+    </KeyboardAvoidingView>
 
-export default AddGroup;
+      </SafeAreaView>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#FFF",
+    },
+    tcontainer: {
+      backgroundColor: "#000",
+      flex:1,
+      flexDirection:'row',
+      padding:50,
+      borderBottomLeftRadius:30,
+      borderBottomRightRadius:30,
+    },
+    bcontainer: {
+        flex: 8,
+        borderTopLeftRadius:90,
+        width:"100%",
+        justifyContent: "center",
+        flexDirection: "column",
+      },
+      tHeading:{
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 20,
+        paddingLeft:"30%",
 
-const styles = StyleSheet.create({
-    Form:{
-        height:500,
-        width:500,
-        color:"#000",
+      },
+    toptcontainer: {
+      backgroundColor: "#3498db",
     },
-    list:{
-        marginTop:300,
+  
+    topbcontainer: {
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "space-around",
     },
-    card:{
-        marginLeft:400,
-        width:400,
-        flexDirection:'row'
+    bottombcontainer: {
+      flex: 1,  
+      flexDirection: "row",
+      justifyContent: "space-around",
     },
-    gradient:{
-        height:'100%',
-        position:"absolute",
-        left:0,
-        right:0,
-        top:0,
-        paddingHorizontal:20,
-        paddingTop:30
+    menuContainer: {
+      justifyContent: "space-around",
+      alignItems: "center",
+      height: "100%",
+      padding: 30,
+      width: "100%",
+      backgroundColor: "#fff",
+      borderRadius: 26,
     },
-    headerContainer:{
-        flexDirection:'row',
-        alignItems:'center'
+    activemenu: {
+      justifyContent: "center",
+      alignItems: "center",
+      height: "80%",
+      width: "45%",
+      backgroundColor: "#17C37B",
+      borderRadius: 10,
     },
-    header:{
-        fontFamily:'Montserrat_800ExtraBold',
-        color:'#FFF',
-        flex:1,
-        fontSize:24
+    activemenuText: {
+      fontSize: 20,
+      color: "#000000",
+      marginTop: 5,
     },
-    proContainer:{
-        marginRight:-20,
-        alignSelf:'center'
+    aText: {
+      color: "#FFFFFF",
+      fontSize: 20,
+      marginTop: 5,
     },
-    ops:{
-        borderTopLeftRadius:40,
-        borderTopRightRadius:40,
-        height:"100%",
-        backgroundColor:'#FFF',
-        marginHorizontal:-20
-    },
-    col:{
-        flexDirection:'row',
-        marginTop:25,
-        marginHorizontal:20,
-        alignItems:'center'
-    },
-    day:{
-        fontFamily:'Montserrat_800ExtraBold',
-        color:'#000119',
-        flex:1,
-        fontSize:20
-    }
-})
+    
+    inputs: {
+        
+        marginTop: Platform.OS === 'ios' ? 0 : -12,
+        paddingLeft: 10,
+        color: '#05375a',
+        width:"90%",
+        height:"10%",
+    borderWidth: 1,
+    borderColor: 'grey',
+
+},
+  });
+  
